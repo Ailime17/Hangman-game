cafb7b4366053d61fe9a9d6f61abda96ac3ed8e7
Emilia <emilia.skrzypczak.es@gmail.com>
2022-10-24 15:16:35 +0200

    Add module for saving & opening saved game

--- script.rb
+++ script.rb
@@ -1,7 +1,66 @@
+require 'yaml'
+
+module YamlHangman
+  def ask_to_open_saved_game
+    while @answer2 != 'y' && @answer2 != 'n'
+      puts 'Open saved game? y/n'
+      @answer2 = gets.strip.downcase
+    end
+    return unless @answer2 == 'y'
+
+    open_saved_game
+    guess_rounds
+  end
+
+  def open_saved_game
+    game_file = File.open('saved_game.yaml', 'r')
+    yaml = game_file.read
+    file = YAML.safe_load(yaml)#, permitted_classes: [Hangman])
+    # file = YAML.safe_load(File.read('saved_game.yaml'))
+    @computer = file['computer']
+    @place_for_word = file['place_for_word']
+    @name = file['name']
+    @incorrect_letters = file['incorrect_letters']
+    @remaining_guesses = file['remaining_guesses']
+    @user_won = file['user_won']
+  end
+
+  def ask_to_save_game
+    @answer1 = ''
+    while @answer1 != 'y' && @answer1 != 'n'
+      puts 'Save game? y/n'
+      @answer1 = gets.strip.downcase
+    end
+    save_game if @answer1 == 'y'
+  end
+
+  def save_game
+    yaml = YAML.dump(
+      'computer' => @computer,
+      'place_for_word' => @place_for_word,
+      'name' => @name,
+      'incorrect_letters' => @incorrect_letters,
+      'remaining_guesses' => @remaining_guesses,
+      'user_won' => @user_won
+    )
+    game_file = File.open('saved_game.yaml', 'w')
+    game_file.write(yaml)
+    game_file.close
+  end
+end
+
 class Hangman
-  def initialize(name)
-    @name = name
-    # puts "Hello #{@name}! Welcome to the hangman game. Try to guess the letter or the full word. You have 10 guesses"
+  include YamlHangman
+
+  def initialize
+    @answer2 = ''
+    ask_to_open_saved_game if File.exist?('saved_game.yaml')
+    return if @answer2 == 'y'
+
+    print 'Enter your name to play: '
+    @name = gets.strip
+    puts "Hello #{@name}! Welcome to the hangman game. Try to guess the letter or the full word. You have 10 guesses."
+    puts "To exit game, type 'exit'."
     @computer = computer_choice
     puts @computer
     @place_for_word = '_' * @computer.length
@@ -27,7 +86,7 @@ class Hangman
 
   def user_won
     @user_won = true
-    puts 'You win!'
+    puts "You win, #{@name}!"
   end
 
   def swap_dashes_for_letters(the_guess)
@@ -71,18 +130,25 @@ class Hangman
 
   def guess_rounds
     while @remaining_guesses.positive? && @user_won == false
+      ask_to_save_game unless @remaining_guesses == 10
       display_word(@place_for_word)
       puts "Remaining guesses: #{@remaining_guesses}"
       puts "Incorrect letters: #{@incorrect_letters}" unless @incorrect_letters.empty?
       @remaining_guesses -= 1
       print 'Your guess: '
-      user_guess = gets.strip
+      user_guess = gets.strip.downcase
+      break if user_guess == 'exit'
+
       eval_guess(user_guess)
     end
-    display_word(@place_for_word)
+    display_end_game_message
+  end
+
+  def display_end_game_message
+    return if @remaining_guesses.positive? && @user_won == false && (@answer1 == 'y' || @answer2 == 'y')
+
+    display_word(@place_for_word) unless @place_for_word == @computer
     puts "Word to guess was: #{@computer}\nGame over"
   end
 end
-# puts "Enter your name to play:"
-# name = gets.strip
-Hangman.new('e')
+Hangman.new
